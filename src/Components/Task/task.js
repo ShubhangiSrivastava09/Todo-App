6dbb317570821ce97b47c321985ddeb8cfd89ff4
CodeEditors <mail2codeeditors@gmail.com>
2024-04-04 01:16:07 +0530

    Checkbox function

--- src/Components/Task/task.js
+++ src/Components/Task/task.js
@@ -8,9 +8,21 @@ function Task(props) {
 
   const {taskList, setTaskList, completedTask, setCompleteTask} = useContext(TaskContext)
 
+  const handleChange = (e) =>{
+    const filterTask = taskList.map((item, index) => {
+        if(index === props.index  ){
+          if(e.target.checked){
+            item.checked = true
+          }
+        }
+        return item;
+    })
+    console.log(filterTask)
+  }
+
   return (
     <Box className={styles.container}>
-      <Checkbox key={props.index} checked={props.checked} color="success" />
+      <Checkbox checked={props.checked} onChange={(e) => handleChange(e)}/>
       <Box className={styles.detailContainer}>
         <Typography variant="body1">{props.name}</Typography>
         <Typography variant="body1">{props.email}</Typography>
